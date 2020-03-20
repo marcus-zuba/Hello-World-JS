2215ea3c3b39a6560a80289199d93a43d658d983
Marcus Vinicius <49821573+marcus-zuba@users.noreply.github.com>
2020-03-20 14:06:35 -0300

    Working with JS

---
+++ js.js
@@ -0,0 +1,38 @@
+function myMove(){
+  var elem = document.getElementById("Animation1");
+  var vpos = 0;
+  var hpos = 0;
+  var id = setInterval(frame, 10);
+  var vdir = 0;
+  var hdir = 0;
+  var initial=1;
+  function frame() {
+      if(vdir==0){
+        vpos++;
+        elem.style.top = 120 + vpos + 'px';
+      }
+      else{
+        vpos--;
+        elem.style.top = 120 + vpos + 'px';
+      }
+      if(hdir==0){
+        hpos++;
+        elem.style.left = hpos + 'px';
+      }
+      else{
+        hpos--;
+        elem.style.left = hpos + 'px';
+      }
+      if(vpos==((window).innerHeight))
+        vdir=1;
+      if(vpos==120)
+        vdir=0;
+      if(hpos==((window).innerWidth))
+        hdir=1;
+      if(hpos==0 && initial==0)
+        hdir=0;
+      if(initial==1)
+        initial=0;
+  }
+
+}
