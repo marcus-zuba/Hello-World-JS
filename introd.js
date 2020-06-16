fd50eb13f2ba22c8bb7f2074a8284aaf608d24d6
Marcus Vinicius <49821573+marcus-zuba@users.noreply.github.com>
2020-06-15 22:40:27 -0300

    Changing little things in JavaScript code

--- introd.js
+++ introd.js
@@ -3,18 +3,18 @@ function mudarConteudoMotivacao(indice) {
     let h2 = document.createElement("H2");
     let p = document.createElement("P");
     let tp;
-    if (indice == 0) {
+    if (indice === 0) {
         tp = document.createTextNode("Que as matrizes são muito utilizadas em diversas áreas a maioria das pessoas sabe. " +
             "O que não é óbvio pra todo mundo é que algumas operações com matrizes só podem ser realizadas com determinantes! " +
             "Dois exemplos disso são a Matriz Inversa e o Cálculo de Autovalores e Autovetores, " +
             "que possuem muitas aplicações na engenharia.");
-    } else if (indice == 1) {
+    } else if (indice === 1) {
         tp = document.createTextNode("Os sistemas lineares são ferramentas matemáticas extremamente importantes em " +
             "diversas áreas. Seja em equações diferenciais de engenharia, seja em equações de balanceamento na química, os " +
             "sistemas lineares são essenciais para a resolução desses problemas. O melhor disso tudo é que os determinantes " +
             "podem ser utilizados para facilitar (e muito) esses sistemas lineares, sendo necessários em diversos momentos " +
             "dos cálculos matemáticos!");
-    } else if (indice == 2) {
+    } else if (indice === 2) {
         tp = document.createTextNode("Os Determinantes também são muito úteis na geometria! Triângulos são " +
             "constantemente usados para a resolução de problemas geométricos, e com a ajuda de determinantes, é " +
             "possível calcular a área desses polígonos de maneira bem simples!");
@@ -34,7 +34,7 @@ function selecionarMotivacao(evt) {
     let menu = document.querySelector('#menu-motivacao');
     let itensMenu = menu.childNodes;
     for (let itemMenu of itensMenu) {
-        if (itemMenu.tagName == "LI") {
+        if (itemMenu.tagName === "LI") {
             itemMenu.classList.remove('menu-motivacao-selecionado');
         }
     }
@@ -51,7 +51,7 @@ function selecionarMotivacao(evt) {
 let menu = document.querySelector('#menu-motivacao');
 let itensMenu = menu.childNodes;
 for (let itemMenu of itensMenu) {
-    if (itemMenu.tagName == "LI") {
+    if (itemMenu.tagName === "LI") {
         itemMenu.addEventListener('click', selecionarMotivacao);
     }
 }
@@ -81,14 +81,14 @@ $inputMatriz = $(".matriz-value");
 
 let foiMudado = false;
 
-function processaMudancaMatriz(evt) {
-    if (foiMudado == false) {
+function processaMudancaMatriz() {
+    if (foiMudado === false) {
         foiMudado = true;
         exibirConclusao();
     }
     let root = document.documentElement;
     let text = $inputMatriz.val();
-    if (text.length != 0) {
+    if (text.length !== 0) {
         root.style.setProperty('--largura', (text.length));
     } else {
         root.style.setProperty('--largura', (1));
